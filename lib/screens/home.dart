51315cefdf437a74fad686ddd74f9401e1bf4432
masterUNG <phrombutr@gmail.com>
2020-02-08 13:48:45 +0700

    BottomNavigator Success

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:ungshoppee/utility/my_style.dart';
+import 'package:ungshoppee/widget/about_user.dart';
+import 'package:ungshoppee/widget/basket.dart';
+
+import 'package:ungshoppee/widget/main_home.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -8,8 +12,50 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   // Field
+  int index = 0;
+  List<Widget> currentWidgets = [
+    MainHome(),
+    Basket(),
+    AboutUser(),
+  ];
 
   // Method
+  Widget myBottomNavigator() {
+    return BottomNavigationBar(onTap: (int i){
+      setState(() {
+        index = i;
+      });
+    },
+      currentIndex: index,
+      items: <BottomNavigationBarItem>[
+        mainHomeNav(),
+        basketNav(),
+        aboutUserNav(),
+      ],
+    );
+  }
+
+  BottomNavigationBarItem mainHomeNav() {
+    return BottomNavigationBarItem(
+      icon: Icon(Icons.home),
+      title: Text('Home'),
+    );
+  }
+
+  BottomNavigationBarItem basketNav() {
+    return BottomNavigationBarItem(
+      icon: Icon(Icons.shopping_cart),
+      title: Text('Basket'),
+    );
+  }
+
+  BottomNavigationBarItem aboutUserNav() {
+    return BottomNavigationBarItem(
+      icon: Icon(Icons.account_box),
+      title: Text('User'),
+    );
+  }
+
   Widget searchButton() {
     return IconButton(
       icon: Icon(Icons.search),
@@ -20,9 +66,12 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(actions: <Widget>[searchButton()],
+      appBar: AppBar(
+        actions: <Widget>[searchButton()],
         title: Text(MyStyle().appName),
       ),
+      body: currentWidgets[index],
+      bottomNavigationBar: myBottomNavigator(),
     );
   }
 }
---
+++ lib/widget/about_user.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AboutUser extends StatefulWidget {
+  @override
+  _AboutUserState createState() => _AboutUserState();
+}
+
+class _AboutUserState extends State<AboutUser> {
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'This is About User'
+    );
+  }
+}
