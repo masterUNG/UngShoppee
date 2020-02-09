a48c786d27c5d65eec1c5bf5a2dcc0ad624b7b74
masterUNG <phrombutr@gmail.com>
2020-02-09 16:04:27 +0700

    finish 9Feb

--- lib/widget/about_user.dart
+++ lib/widget/about_user.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:ungshoppee/models/user_model.dart';
 
 class AboutUser extends StatefulWidget {
   @override
@@ -6,10 +10,160 @@ class AboutUser extends StatefulWidget {
 }
 
 class _AboutUserState extends State<AboutUser> {
+  // Field
+  String name, user, password;
+  bool status = true;
+
+  // Method
+  Widget registerButton() {
+    return Container(
+      width: 250.0,
+      child: OutlineButton(
+        child: Text('Register'),
+        onPressed: () {
+          if (name == null ||
+              name.isEmpty ||
+              user == null ||
+              user.isEmpty ||
+              password == null ||
+              password.isEmpty) {
+            print('Have Space');
+          } else {
+            registerThread();
+          }
+        },
+      ),
+    );
+  }
+
+  Future<void> registerThread() async {
+    String url =
+        'https://www.androidthai.in.th/ong/addUserMaster.php?isAdd=true&Name=$name&User=$user&Password=$password';
+
+    Response response = await Dio().get(url);
+    if (response.toString() == 'true') {
+      print('Register Success');
+      setState(() {
+        status = false;
+      });
+    } else {
+      print('Cannot Register');
+    }
+  }
+
+  Widget nameForm() {
+    return Container(
+      child: TextField(
+        onChanged: (String string) {
+          name = string.trim();
+        },
+        decoration: InputDecoration(
+          hintText: 'Display Name:',
+          prefixIcon: Icon(Icons.face),
+        ),
+      ),
+      width: 250.0,
+    );
+  }
+
+  Widget passwordForm() {
+    return Container(
+      child: TextField(
+        onChanged: (String string) {
+          password = string.trim();
+        },
+        decoration: InputDecoration(
+          hintText: 'Password:',
+          prefixIcon: Icon(Icons.lock),
+        ),
+      ),
+      width: 250.0,
+    );
+  }
+
+  Widget userForm() {
+    return Container(
+      child: TextField(
+        onChanged: (String string) {
+          user = string.trim();
+        },
+        decoration: InputDecoration(
+          hintText: 'User:',
+          prefixIcon: Icon(Icons.account_box),
+        ),
+      ),
+      width: 250.0,
+    );
+  }
+
+  Widget loginButton() {
+    return Container(
+      width: 250.0,
+      child: RaisedButton(
+        child: Text('Login'),
+        onPressed: () {
+
+          if (user.isEmpty || password.isEmpty) {
+            print('Have Space');
+          } else {
+            checkAuthen();
+          }
+
+        },
+      ),
+    );
+  }
+
+  Future<void> checkAuthen()async{
+
+    String url = 'https://www.androidthai.in.th/ong/getUserWhereUser.php?isAdd=true&User=$user';
+    Response response = await Dio().get(url);
+
+    if (response.toString() == 'null') {
+      print('User False');
+    } else {
+
+      var result = json.decode(response.data);
+      for (var map in result) {
+        UserModel userModel = UserModel.fromJson(map);
+        
+      }
+
+
+    }
+
+
+  }
+
+  Widget authenForm() {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          userForm(),
+          passwordForm(),
+          loginButton(),
+        ],
+      ),
+    );
+  }
+
+  Widget registerForm() {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          nameForm(),
+          userForm(),
+          passwordForm(),
+          registerButton(),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text(
-      'This is About User'
-    );
+    return status ? registerForm() : authenForm();
   }
 }
